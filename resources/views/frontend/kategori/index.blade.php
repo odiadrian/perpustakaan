a2c383a3cc9a4146e155e0dcd4cda45dfbacdbb5
odiadrian <adrianaodi592@gmail.com>
2023-11-23 15:21:11 +0700

    .

--- resources/views/frontend/kategori/index.blade.php
+++ resources/views/frontend/kategori/index.blade.php
@@ -11,12 +11,12 @@
         </div>
         <div class="row">
             <!-- Category list -->
-            @foreach($buku as $book)
+            @foreach($kategori as $kate)
             <div class="col-lg-3 offset-lg-0 col-md-5 offset-md-1 col-sm-6 col-6">
                 <div class="category-block">
                     <div class="header">
                         <i class="fa fa-laptop icon-bg-1"></i>
-                        <a href="">
+                        <a href="{{ route('frontend.show.kategori', $kate->slug) }}">
                             <h4>Biografi </h4>
                         </a>
                     </div>
